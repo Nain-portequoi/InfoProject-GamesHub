2085d2eff14fb7d4d065b9abf5f5a7fa9c21ddb5
Mangabeyyy <delhaxhepierre@gmail.com>
2026-04-24 15:16:51 +0200

    Test Ajout de tous

---
+++ Memory_Form.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Memory_Pierre
+{
+    partial class Memory_Form
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCard1 = new System.Windows.Forms.Button();
+            this.BtnCard2 = new System.Windows.Forms.Button();
+            this.BtnCard3 = new System.Windows.Forms.Button();
+            this.BtnCard4 = new System.Windows.Forms.Button();
+            this.BtnCard5 = new System.Windows.Forms.Button();
+            this.BtnCard6 = new System.Windows.Forms.Button();
+            this.BtnCard7 = new System.Windows.Forms.Button();
+            this.BtnCard8 = new System.Windows.Forms.Button();
+            this.BtnCard9 = new System.Windows.Forms.Button();
+            this.BtnCard10 = new System.Windows.Forms.Button();
+            this.BtnCard11 = new System.Windows.Forms.Button();
+            this.BtnCard12 = new System.Windows.Forms.Button();
+            this.BtnCard13 = new System.Windows.Forms.Button();
+            this.BtnCard14 = new System.Windows.Forms.Button();
+            this.BtnCard15 = new System.Windows.Forms.Button();
+            this.BtnCard16 = new System.Windows.Forms.Button();
+            this.BtnCard17 = new System.Windows.Forms.Button();
+            this.BtnCard18 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // BtnCard1
+            // 
+            this.BtnCard1.Location = new System.Drawing.Point(82, 90);
+            this.BtnCard1.Name = "BtnCard1";
+            this.BtnCard1.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard1.TabIndex = 0;
+            this.BtnCard1.UseVisualStyleBackColor = true;
+            this.BtnCard1.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard2
+            // 
+            this.BtnCard2.Location = new System.Drawing.Point(262, 90);
+            this.BtnCard2.Name = "BtnCard2";
+            this.BtnCard2.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard2.TabIndex = 1;
+            this.BtnCard2.UseVisualStyleBackColor = true;
+            this.BtnCard2.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard3
+            // 
+            this.BtnCard3.Location = new System.Drawing.Point(441, 90);
+            this.BtnCard3.Name = "BtnCard3";
+            this.BtnCard3.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard3.TabIndex = 2;
+            this.BtnCard3.UseVisualStyleBackColor = true;
+            this.BtnCard3.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard4
+            // 
+            this.BtnCard4.Location = new System.Drawing.Point(626, 90);
+            this.BtnCard4.Name = "BtnCard4";
+            this.BtnCard4.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard4.TabIndex = 3;
+            this.BtnCard4.UseVisualStyleBackColor = true;
+            this.BtnCard4.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard5
+            // 
+            this.BtnCard5.Location = new System.Drawing.Point(805, 90);
+            this.BtnCard5.Name = "BtnCard5";
+            this.BtnCard5.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard5.TabIndex = 4;
+            this.BtnCard5.UseVisualStyleBackColor = true;
+            this.BtnCard5.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard6
+            // 
+            this.BtnCard6.Location = new System.Drawing.Point(979, 90);
+            this.BtnCard6.Name = "BtnCard6";
+            this.BtnCard6.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard6.TabIndex = 5;
+            this.BtnCard6.UseVisualStyleBackColor = true;
+            this.BtnCard6.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard7
+            // 
+            this.BtnCard7.Location = new System.Drawing.Point(82, 259);
+            this.BtnCard7.Name = "BtnCard7";
+            this.BtnCard7.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard7.TabIndex = 6;
+            this.BtnCard7.UseVisualStyleBackColor = true;
+            this.BtnCard7.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard8
+            // 
+            this.BtnCard8.Location = new System.Drawing.Point(262, 259);
+            this.BtnCard8.Name = "BtnCard8";
+            this.BtnCard8.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard8.TabIndex = 7;
+            this.BtnCard8.UseVisualStyleBackColor = true;
+            this.BtnCard8.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard9
+            // 
+            this.BtnCard9.Location = new System.Drawing.Point(441, 259);
+            this.BtnCard9.Name = "BtnCard9";
+            this.BtnCard9.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard9.TabIndex = 8;
+            this.BtnCard9.UseVisualStyleBackColor = true;
+            this.BtnCard9.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard10
+            // 
+            this.BtnCard10.Location = new System.Drawing.Point(626, 259);
+            this.BtnCard10.Name = "BtnCard10";
+            this.BtnCard10.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard10.TabIndex = 9;
+            this.BtnCard10.UseVisualStyleBackColor = true;
+            this.BtnCard10.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard11
+            // 
+            this.BtnCard11.Location = new System.Drawing.Point(805, 259);
+            this.BtnCard11.Name = "BtnCard11";
+            this.BtnCard11.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard11.TabIndex = 10;
+            this.BtnCard11.UseVisualStyleBackColor = true;
+            this.BtnCard11.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard12
+            // 
+            this.BtnCard12.Location = new System.Drawing.Point(979, 259);
+            this.BtnCard12.Name = "BtnCard12";
+            this.BtnCard12.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard12.TabIndex = 11;
+            this.BtnCard12.UseVisualStyleBackColor = true;
+            this.BtnCard12.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard13
+            // 
+            this.BtnCard13.Location = new System.Drawing.Point(82, 435);
+            this.BtnCard13.Name = "BtnCard13";
+            this.BtnCard13.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard13.TabIndex = 12;
+            this.BtnCard13.UseVisualStyleBackColor = true;
+            this.BtnCard13.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard14
+            // 
+            this.BtnCard14.Location = new System.Drawing.Point(262, 435);
+            this.BtnCard14.Name = "BtnCard14";
+            this.BtnCard14.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard14.TabIndex = 13;
+            this.BtnCard14.UseVisualStyleBackColor = true;
+            this.BtnCard14.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard15
+            // 
+            this.BtnCard15.Location = new System.Drawing.Point(441, 435);
+            this.BtnCard15.Name = "BtnCard15";
+            this.BtnCard15.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard15.TabIndex = 14;
+            this.BtnCard15.UseVisualStyleBackColor = true;
+            this.BtnCard15.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard16
+            // 
+            this.BtnCard16.Location = new System.Drawing.Point(626, 435);
+            this.BtnCard16.Name = "BtnCard16";
+            this.BtnCard16.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard16.TabIndex = 15;
+            this.BtnCard16.UseVisualStyleBackColor = true;
+            this.BtnCard16.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard17
+            // 
+            this.BtnCard17.Location = new System.Drawing.Point(805, 435);
+            this.BtnCard17.Name = "BtnCard17";
+            this.BtnCard17.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard17.TabIndex = 16;
+            this.BtnCard17.UseVisualStyleBackColor = true;
+            this.BtnCard17.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // BtnCard18
+            // 
+            this.BtnCard18.Location = new System.Drawing.Point(979, 435);
+            this.BtnCard18.Name = "BtnCard18";
+            this.BtnCard18.Size = new System.Drawing.Size(112, 101);
+            this.BtnCard18.TabIndex = 17;
+            this.BtnCard18.UseVisualStyleBackColor = true;
+            this.BtnCard18.Click += new System.EventHandler(this.AnyButtonClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(92, 587);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 32);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Joueur 1 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(661, 587);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 32);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Joueur 2 :";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(274, 596);
+            this.progressBar1.Maximum = 5;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 20;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(851, 596);
+            this.progressBar2.Maximum = 5;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(100, 23);
+            this.progressBar2.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(1208, 670);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnCard18);
+            this.Controls.Add(this.BtnCard17);
+            this.Controls.Add(this.BtnCard16);
+            this.Controls.Add(this.BtnCard15);
+            this.Controls.Add(this.BtnCard14);
+            this.Controls.Add(this.BtnCard13);
+            this.Controls.Add(this.BtnCard12);
+            this.Controls.Add(this.BtnCard11);
+            this.Controls.Add(this.BtnCard10);
+            this.Controls.Add(this.BtnCard9);
+            this.Controls.Add(this.BtnCard8);
+            this.Controls.Add(this.BtnCard7);
+            this.Controls.Add(this.BtnCard6);
+            this.Controls.Add(this.BtnCard5);
+            this.Controls.Add(this.BtnCard4);
+            this.Controls.Add(this.BtnCard3);
+            this.Controls.Add(this.BtnCard2);
+            this.Controls.Add(this.BtnCard1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCard1;
+        private System.Windows.Forms.Button BtnCard2;
+        private System.Windows.Forms.Button BtnCard3;
+        private System.Windows.Forms.Button BtnCard4;
+        private System.Windows.Forms.Button BtnCard5;
+        private System.Windows.Forms.Button BtnCard6;
+        private System.Windows.Forms.Button BtnCard7;
+        private System.Windows.Forms.Button BtnCard8;
+        private System.Windows.Forms.Button BtnCard9;
+        private System.Windows.Forms.Button BtnCard10;
+        private System.Windows.Forms.Button BtnCard11;
+        private System.Windows.Forms.Button BtnCard12;
+        private System.Windows.Forms.Button BtnCard13;
+        private System.Windows.Forms.Button BtnCard14;
+        private System.Windows.Forms.Button BtnCard15;
+        private System.Windows.Forms.Button BtnCard16;
+        private System.Windows.Forms.Button BtnCard17;
+        private System.Windows.Forms.Button BtnCard18;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+    }
+}
+
---
+++ Memory_Form.cs
@@ -0,0 +1,108 @@
+﻿using Memory_Pierre.Modele;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Memory_Pierre
+{
+    public partial class Memory_Form : Form
+    {
+        private ImageCollection _imageCollection;
+        private List<Button> InterfaceButton;
+        private Game _game;
+        int x = 0;
+        public Memory_Form()
+        {
+            InitializeComponent();
+            _imageCollection = new ImageCollection();
+            _game = new Game(_imageCollection);
+            _game.SetUpGame();
+            InterfaceButton = new List<Button> { BtnCard1, BtnCard2, BtnCard3, BtnCard4, BtnCard5, BtnCard6, BtnCard7, BtnCard8, BtnCard9, BtnCard10, BtnCard11, BtnCard12, BtnCard13, BtnCard14, BtnCard15, BtnCard16, BtnCard17, BtnCard18 };
+            ConnectCardToButton(InterfaceButton, _imageCollection.GetImageCollection());
+            PutImageRecto(InterfaceButton);
+        }
+        private void ConnectCardToButton(List<Button> Button, List<Card> Card)
+        {
+            int i;
+            for(i=0;i<Button.Count;i++)
+            {
+                Button[i].Tag = Card[i];
+            }
+        }
+        private void PutImageRecto(List<Button> listOfButton)
+        {
+            foreach(Button BtnCard in listOfButton)
+            {
+                HideImage(BtnCard);
+            }
+
+        }
+        private void ShowImage(Button btn)
+        {
+            
+            Card carte = (Card)btn.Tag;
+            // On va chercher l'image dans les ressources grâce au nom
+            // On utilise Properties.Resources.ResourceManager.GetObject(nom)
+            object ressourceImage = Properties.Resources.ResourceManager.GetObject(carte.GetNameImage());
+
+            
+            if (ressourceImage != null)
+            {
+                btn.BackgroundImage = (Image)ressourceImage;
+                btn.BackgroundImageLayout = ImageLayout.Stretch; // Pour que l'image remplisse le bouton
+            }
+        }
+
+
+        private void HideImage(Button btn)
+        {
+            object ImageRecto = Properties.Resources.ResourceManager.GetObject("Recto");
+            btn.BackgroundImage = (Image)ImageRecto;
+            btn.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+
+        private void AnyButtonClick(object sender, EventArgs e)
+        {
+            Button buttonClicked = (Button)sender;
+            Card CardClicked = (Card)buttonClicked.Tag;
+            _game.SelectCard(CardClicked);
+            /*
+            if(_game.SelectCard(CardClicked) == Game.FlipResult.Match)
+            {
+                // Ajouter des points 
+                // Faire en sorte que si un joueur a gagné, la partie s'arrête ! 
+            }
+            if (_game.SelectCard(CardClicked) == Game.FlipResult.NoMatch)
+            {
+                // Joueur suivant
+            }
+            */
+            ShowAndHideCard(InterfaceButton);
+        }
+
+
+        private void ShowAndHideCard(List<Button> AllButton)
+        {
+            foreach(Button button in AllButton)
+            {
+                Card card = (Card)button.Tag;
+                if(card.GetIsReturn()==true)
+                {
+                    ShowImage(button);
+                }
+                else
+                {
+                    HideImage(button);
+                }
+            }
+        }
+    }
+}
